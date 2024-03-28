0d6b35a667d1e0a89e4e541c913da79c43fa031d
theeffectik <andruxa-mix112@yandex.ru>
2024-03-28 18:25:59 +0300

    update deploying

--- scripts/deployFloatingStakingCollection.ts
+++ scripts/deployFloatingStakingCollection.ts
@@ -7,32 +7,32 @@ export async function run(provider: NetworkProvider) {
         FloatingStaking.createFromConfig(
             {
                 init: 1n, 
-                next_item_index: 1n,
-                last_update_time: 0n,
+                next_item_index: 321n,
+                last_update_time: 1711634270n,
                 admin_address: Address.parse("0QCWVqwkomdw-o4wsVqdBO_HHkv584nZw0ziJUVgeUWG6MkO") as Address,
 
                 nft_item_code: await compile('NftItem'),
                 collection_content: Cell.fromBase64("te6ccgEBAwEAagACAAECAFxodHRwczovL2p2YXVsdC5hcHAvY29sbGVjdGlvbl9jb250ZW50P25hbWU9SlZUAGhodHRwczovL2p2YXVsdC5hcHAvbmZ0X2NvbnRlbnQ/Y29sbGVjdGlvbj1KVlQmaW5kZXg9"),
 
-                last_tvl: 0n,
-                distributed_rewards: 0n, 
+                last_tvl: 909197129509998n,
+                distributed_rewards: 20185462348121548244921499133n, 
 
                 min_lock_period: 0n,
                 farming_speed: 0n, 
 
-                rewards_balance: 0n, 
+                rewards_balance: 250000000999938n, 
                 commission_factor: 0n, 
 
                 lock_wallet_set        : 0n,
                 rewards_wallet_set     : 0n,
-                premint_open           : 0n,
+                premint_open           : 1n,
 
-                start_time             : 0n,
-                end_time               : 0n,
-                minimum_deposit        : 0n,
+                start_time             : 1707251534n,
+                end_time               : 1770336000n,
+                minimum_deposit        : 20000000000n,
 
-                lock_wallet_address    : Address.parse("0QCWVqwkomdw-o4wsVqdBO_HHkv584nZw0ziJUVgeUWG6MkO") as Address,
-                rewards_wallet_address : Address.parse("0QCWVqwkomdw-o4wsVqdBO_HHkv584nZw0ziJUVgeUWG6MkO") as Address,
+                lock_wallet_address    : Address.parse("EQC8FoZMlBcZhZ6Pr9sHGyHzkFv9y2B5X9tN61RvucLRzFZz") as Address,
+                rewards_wallet_address : Address.parse("EQC8FoZMlBcZhZ6Pr9sHGyHzkFv9y2B5X9tN61RvucLRzFZz") as Address,
                 creator_address        : Address.parse("0QCWVqwkomdw-o4wsVqdBO_HHkv584nZw0ziJUVgeUWG6MkO") as Address
 
             },
